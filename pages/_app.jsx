e34575a793720878277e4d2272dc22c728064d01
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2024-06-06 11:53:47 +0100

    feat: Add page title and favicon
    Add a page title and favicon to the application for better branding and user experience.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,14 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <title>Auction Platform</title>
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
   )
 }
